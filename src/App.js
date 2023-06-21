0bb7f551a8b20da435455595b3633b463a455a0b
bakhtin_pavel <p1avel.com@gmial.com>
2023-06-21 23:12:29 +0700

    second test2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path='/' element={<Layout />}>
+                <Route path='/home' element={<Layout />}>
                     <Route index element={<Main />} />
                     <Route path='installation' element={<Installation />} />
                     <Route path='aboutus' element={<AboutUs />} />
