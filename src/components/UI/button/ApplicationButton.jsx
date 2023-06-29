10367f3f28c3afdafdc7b8b1d567506de929712c
bakhtin_pavel <p1avel.com@gmial.com>
2023-06-29 13:35:15 +0700

    test npm install

--- src/components/UI/button/ApplicationButton.jsx
+++ src/components/UI/button/ApplicationButton.jsx
@@ -33,7 +33,6 @@ const ApplicationButton = ({ onClick }) => {
     //     window.addEventListener('scroll', updatePosition);
     //     updatePosition()
     //     return () => window.removeEventListener('scroll', updatePosition);
-
     // })
 
     return (
